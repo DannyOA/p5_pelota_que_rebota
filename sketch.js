f86ed3cc617d26248afa6bb3c64c681bb7e13c7a
Danny Ordoñez <daniel.ordoneza@ucuenca.edu.ec>
2025-11-07 23:58:24 -0500

    se creó un sistema de pelotas

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const NP = 400;
+let pelotas = [];
+
+
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+    rectMode(CENTER);    
+
+    for(let i = 0; i < NP; i++){
+        let nuevaPelota = new Pelota();
+        pelotas[i] = nuevaPelota;
+    }
+
+
+}
+
+function draw() {
+    background(150);
+
+    for(let i = 0; i < NP; i++){
+        pelotas[i].actualizar();
+        pelotas[i].visualizar();
+    }
+}
+
+
